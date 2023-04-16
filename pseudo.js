50d63595d81b93bd5206c0f95d140c5c0e309832
JASaddington <jasaddington@gmail.com>
2023-04-16 13:44:46 -0500

    meeting notes 1

---
+++ pseudo.js
@@ -0,0 +1,38 @@
+//  pseudo code + division of labor
+
+// html skeleton, divs with IDs of placements of features
+  // title for the name of the website
+  // searchbar for ingredients
+    // dropdown for base liquor, fixed options.
+    // input (text) fields for ingredients (forms)
+    // option to delete/clear fields
+    // !garnish options
+    // 40% searchbar area, 60% cocktail cards
+
+  // history local storage for each previous card search
+    // cards on Foundation
+    // try to get 3d presentation
+      // !if not, can do overflow-y horizontal scroll 2D
+    // examples of local storage the weatherAPI app
+    // !maybe have the characters specific to the saved search
+
+  // "about the authors" on separate HTML page, link to bottom of app
+
+// setting up API keys and queries, find what info specifically needed
+  // by ingredients, multi ingredients also.
+  // query variables / arrays for searching for items
+  // fxns for populating cards, append and set attribute
+      // research foundation classes/IDs to create cards
+    
+
+
+
+
+
+
+
+// js fxns to populate HTML divs based on queries
+
+// formatting and finalizing html and css for app.
+  // create custom classes/IDs as needed
+// Slide show
