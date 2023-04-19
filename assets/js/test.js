2f9d1d25b44ddf646f674a2e0506695594ce7215
Marq W <marquisewest86@duck.com>
2023-04-18 22:45:37 -0500

    updated the rickandmortyapi query

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,10 @@
-var rickandmortyURL = "https://rickandmortyapi.com/api/character";
+var page = Math.floor(Math.random() * 42);
+
+console.log(page)
+
+var rickandmortyURL = `https://rickandmortyapi.com/api/character/?page=${page}`;
+var avatar = 2;
+var rickandmortyImage = `https://rickandmortyapi.com/api/character/avatar/${avatar}.jpeg`
 
 fetch(rickandmortyURL, {
     method: 'GET',
